74fffa7adef80cdc3dad32d276cd8821b5ec7d7d
nurfaidzy.riyamizard <nurfaidzy.riyamizard@akulaku.com>
2024-11-25 20:59:57 +0700

    feat: get the statis files

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import rawData from './data/data.json';
+import { join } from 'path';
+
+interface detailData {
+  id: number;
+  name: string;
+  city: string;
+  image: string;
+  rating: number;
+  price: number;
+}
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getImages = (imageUrl: string): string => {
+    const resolvedPath = join(
+      __dirname,
+      '..',
+      'images',
+      imageUrl.split('/').pop(),
+    );
+    return resolvedPath;
+  };
+
+  getListData(): detailData[] {
+    rawData.forEach((item) => {
+      item.image = this.getImages(item.image);
+    });
+    return rawData;
   }
 }
---
+++ src/interfaces/location.interfaces.ts
@@ -0,0 +1,8 @@
+export interface detailData {
+  id: number;
+  name: string;
+  city: string;
+  image: string;
+  rating: number;
+  price: number;
+}
